6a89ee5cdf1a15f61e8db493e0e8fb5fe1e163b8
Sumi111 <79397105+Sumi111@users.noreply.github.com>
2021-11-24 14:52:56 +0400

    ft_printf.c
    for d, s and x only

---
+++ ft_printf.c
@@ -0,0 +1,90 @@
+#include<stdarg.h>
+#incude<unistd.h>
+
+int ft_print_char(char c)
+{
+  write(1, &c, 1);
+  ct++;
+  return (ct);
+}
+
+int ft_print_int(long n, int ct)
+{
+  if (n < 0)
+  {
+    write(1, "-", 1);
+    ct++;
+    n = -n;
+  }
+  if (n >= 10)
+    ct = ft_print_int(n /10, ct);
+  ct = ft_print_char(n % 10 + '0', ct);
+}
+
+int ft_strlen(char *str)
+{
+  int i = 0;
+  
+  while (str[i])
+    i++;
+  return (i);
+}
+
+int ft_print_str(char *str, int ct)
+{
+  if (str)
+  {
+    write(1, str, ft_strlen(str));
+    ct += ft_strlen(str);
+  }
+  else
+    ct = ft_print_str("(null)");
+  return (ct);
+}
+
+int ft_print_hex(unsigned int n, int ct)
+{
+  if (n >= 16)
+    ct = ft_print_hex(n / 16, ct);
+  n = n % 16;
+  if (n < 10)
+    ct = ft_print_char(n + '0', ct);
+  else
+    ct = ft_print_char(n + 87, ct);
+  return (ct);
+}
+
+int ft_arg(char c, va_list ap, int ct)
+{
+  if (c == 'd')
+    ct = ft_print_int(va_arg(ap, int), ct);
+  else if (c == 's')
+    ct = ft_print_str(va_arg(ap, char *), ct);
+  else if (c == 'x')
+    ct = ft_print_hex(va_arg(ap, unsigned int), ct);
+  return (ct);
+}
+
+int ft_printf(const char *str, ...)
+{
+  int i;
+  int ct;
+  va_list ap;
+  
+  i = 0;
+  ct = 0;
+  va_start(ap, str);
+  while (str[i])
+  {
+    if (str[i] != '%')
+    {
+      write(1, str[i], 1);
+      ct++;
+    }
+    else
+      ct = ft_arg(str[i], ap, ct);
+    i++;
+  }
+  va_end(ap);
+  return(ct);
+}
